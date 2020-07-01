c73ffa8bd7ff503168a19314b37b71cfacdb63ae
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-07-01 10:31:00 +0700

    themthamso

--- bài 4.js
+++ bài 4.js
@@ -1,15 +1,16 @@
-let Circle = function (radius,x,y) {
+let Circle = function (radius,x,y,color) {
     this.radius = radius;
     this.x = x;
     this.y = y;
+    this.color = color;
     this.renderCircle = function (circle) {
         let canvas = document.getElementById("canvas");
         let ctx = canvas.getContext("2d")
         ctx.arc(circle.x, circle.y,  circle.radius, 0, 2*Math.PI);
-        ctx.fillStyle = "#000000";
+        ctx.fillStyle = circle.color;
         ctx.fill()
     }
 }
 
-let circle = new Circle(100, 10, 10);
+let circle = new Circle(100, 10, 10, "#000000");
 circle.renderCircle(circle);
